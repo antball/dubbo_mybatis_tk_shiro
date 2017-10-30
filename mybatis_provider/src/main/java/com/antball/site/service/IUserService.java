6dd1ee089c8f012e40ba9d3834d098f2a6ecc428
antball <antball@163.com>
2017-10-30 18:02:03 +0800

    add

---
+++ mybatis_provider/src/main/java/com/antball/site/service/IUserService.java
@@ -0,0 +1,12 @@
+package com.antball.site.service;
+
+import com.antball.site.model.TUser;
+
+/**
+ * Created by DELL on 2017/10/30.
+ */
+public interface IUserService {
+
+
+    public TUser getUserById(int id);
+}
